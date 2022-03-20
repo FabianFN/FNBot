1c3bec19a1f0056bd4b2c919f7e5cee667e91fb6
FabianFN <96866412+FabianFN@users.noreply.github.com>
2022-03-20 16:19:22 -0500

    Update instalarbot.js

--- plugins/instalarbot.js
+++ plugins/instalarbot.js
@@ -1,7 +1,10 @@
 let handler = async m => m.reply(`
 *◄┢┅͜͡✇⟬↯ື ►∉𝓕𝓪͢͢͢𝓫𝓲𝓪𝓷∌◄ ↯ື⟭✇͜͡┅┧►*
 
+*◄┢┅͜͡✇⟬↯ື ►∉𝓕𝓪͢͢͢𝓫𝓲𝓪𝓷∌◄ ↯ື⟭✇͜͡┅┧►*
+
 💥 𝐑𝐄𝐐𝐔𝐈𝐒𝐈𝐓𝐎𝐒 𝐏𝐀𝐑𝐀 𝐋𝐀 𝐈𝐍𝐒𝐓𝐀𝐋𝐀𝐂𝐈𝐎𝐍 💥
+➤ Tutorial completo: 
 ➤ Termux  
 ➤ 1 GB de memoria en el teléfono
 ➤ Aconsejable un WhatsApp secundario
@@ -9,24 +12,26 @@ let handler = async m => m.reply(`
 ➤ 2 teléfonos o un 1 teléfono y una PC (es temporal - solo para escanear un código QR)
 
 💥 𝐀𝐑𝐄𝐀 𝐃𝐄 𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀𝐒 💥
-➤ Termux: https://f-droid.org/en/packages/com.termux/
+➤ Termux: https://www.mediafire.com/download/79r29qab48tm0fo
 ➤ WhatsApp recomendado: https://www.mediafire.com/file/j5vmyudprv012ol/%25E2%259E%25A2_SOLITARIO_BUSINESS_%25E2%259E%25A2_2.21.19.21.apk/file
    
 💥 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐃𝐄 𝐈𝐍𝐒𝐓𝐀𝐋𝐀𝐂𝐈𝐎𝐍 𝐕𝐈𝐀 𝐆𝐈𝐓𝐇𝐔𝐁 💥
-➤ apt update && apt upgrade -y -y
-➤ pkg install nodejs -y
-➤ pkg install ffmpeg -y
-➤ pkg install imagemagick -y
-➤ pkg install npm 
-➤ termux-setup-storage
-➤ cd storage/downloads/FNBot
-➤ npm start
+➤ pkg install ffmpeg
+➤ pkg install imagemagick
+➤ apt update
+➤ apt upgrade
+➤ pkg install wget
+➤ pkg install git 
+➤ pkg install nodejs
+➤ pkg install npm
+➤ git clone https://github.com/FabianFN/FNBot
+➤ cd FNBot
+➤ npm start 
 
 ° Escaneas el código QR, tienes 30 segundos para hacerlo
 
 💥 INFO EXTRA 💥
 - Puede ocasionar que el numero se vaya a soporte 
-➤ PayPal para apoyo voluntario: https://www.paypal.me/fabianxd692
 ➤ Numero de ayuda durante la instalacion: wa.me/51933748331 (No Bot, solo ayuda durante la instalación)
 
 *◄┢┅͜͡✇⟬↯ື ►ஜ۩💥۩ஜ◄ ↯ື⟭✇͜͡┅┧►*
