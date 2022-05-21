885ea010b4eff60175040fd00482bc243f8953a2
FabianFN <96866412+FabianFN@users.noreply.github.com>
2022-05-21 16:12:07 -0500

    ©F

--- plugins/yuri.js
+++ plugins/yuri.js
@@ -1,23 +1,8 @@
-const axios = require('axios')
- let handler = async(m, { conn }) => {
-let les = await axios.get('https://meme-api.herokuapp.com/gimme/yuri')
-            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
-  }
-handler.help = ['yuri']
-handler.tags = ['General']
+let fetch = require('node-fetch')
+let handler  = async (m, { conn, usedPrefix, command }) => {
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
+heum = await fetch(`https://api-alc.herokuapp.com/api/nsfw/yuri2?apikey=ConfuMods`)
+json = await heum.buffer()
+conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©FNBot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
 handler.command = /^(yuri)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = false
-
 module.exports = handler
